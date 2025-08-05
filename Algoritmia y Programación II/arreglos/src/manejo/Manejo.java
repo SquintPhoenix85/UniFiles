ff87929e078a81086c006f492ec67fff236203fb
SquintPhoenix85 <108373281+SquintPhoenix85@users.noreply.github.com>
2025-08-05 09:21:15 -0500

    Update Manejo.java

--- Algoritmia y Programación II/arreglos/src/manejo/Manejo.java
+++ Algoritmia y Programación II/arreglos/src/manejo/Manejo.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package manejo;
 
 import java.util.Scanner;
@@ -12,9 +9,6 @@ import java.util.Scanner;
  */
 public class Manejo {
 
-    /**
-     * @param args the command line arguments
-     */
     
     public static void main(String[] args) {
         // Create Scanner for System.in - Allows user input
@@ -57,3 +51,4 @@ public class Manejo {
     }
     
 }
+
